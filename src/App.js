7a6c51941a8097fd1d1a93c77d3e6a847a943efc
nirmala <nirmala@surf.net.np>
2018-11-11 09:14:49 +0100

    create welcome component and add conditional rendering in app component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React, { Component } from 'react';
-import Home from './Home';
+import React, { Component } from "react";
+import Home from "./Home";
+import Welcome from "./Welcome";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      user: 'Neer'
-    }
+      user: "n"
+    };
   }
   render() {
-    return <Home user={this.state.user}/>;
+    return (
+      <div>
+        {this.state.user && <Welcome user={this.state.user} />}        
+        <Home user={this.state.user} />
+      </div>
+    );
   }
 }
 
