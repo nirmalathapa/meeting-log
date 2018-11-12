44c5bdeb467c56f7e9b5c4fd2f5f5c4cd9846c62
nirmala <nirmala@surf.net.np>
2018-11-12 14:57:30 +0100

    add components login, meeting and register

---
+++ src/Meetings.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Meeting extends Component {
+  render() {
+    return (
+      <div className="text-center mt-4">
+        <h1 className="text-primary">Login</h1>
+      </div>
+    );
+  }
+}
+
+export default Meeting;
