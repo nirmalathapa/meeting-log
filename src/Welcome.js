012d7eb52f765fcf018ca3200beae8c0651386cb
nirmala <nirmala@surf.net.np>
2018-11-13 21:36:05 +0100

    change a tag to link and add onclick event

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,17 +1,19 @@
 import React, { Component } from "react";
+import { Link } from "@reach/router";
 
 class Welcome extends Component {
   render() {
-    const { user } = this.props;
+    const { userName, logOutUser } = this.props;
     return (
       <div className="text-center mt-4">
         <span className="text-secondary font-weight-bold pl-1">
-          Welcome {user}
+          Welcome {userName}
         </span>
         ,
-        <a href="/" className="font-weight-bold text-primary pl-1">
+        <Link to="/login" className="font-weight-bold text-primary pl-1"
+        onClick={e => logOutUser(e)}>
         {''} Logout
-        </a>
+        </Link>
       </div>
     );
   }
